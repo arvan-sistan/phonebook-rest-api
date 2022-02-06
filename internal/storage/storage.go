e1eb6c767985595ba8d00b1a4bbc501d6b6d76d3
MehdiAzari <lolaccmahdi2@gmail.com>
2022-02-06 23:22:48 +0330

    impelemented storage interfaces

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -11,8 +11,6 @@ import (
 var (
 	ErrorUserNotFound  = errors.New("user with given username doesn't exist")
 	ErrorUserDuplicate = errors.New("user with given username already exists")
-
-	ErrorMaxUrlCount = errors.New("user reached max url count")
 )
 
 type User interface {
